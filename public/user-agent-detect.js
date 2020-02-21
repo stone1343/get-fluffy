98409a0c9eaa5fb1d3807c50189fb48c324e0576
Jeff Stone <stone1343@users.noreply.github.com>
2020-02-21 15:34:51 -0500

    Update user-agent-detect.js

--- public/user-agent-detect.js
+++ public/user-agent-detect.js
@@ -1,6 +1,7 @@
 // From http://stackoverflow.com/questions/11381673/detecting-a-mobile-browser 2017-02-05
 
 window.onload = userAgentDetect;
+
 function userAgentDetect() {
   if(window.navigator.userAgent.match(/Mobile/i)
   || window.navigator.userAgent.match(/iPhone/i)
@@ -9,25 +10,12 @@ function userAgentDetect() {
   || window.navigator.userAgent.match(/Windows Phone/i)
   || window.navigator.userAgent.match(/Android/i)
   || window.navigator.userAgent.match(/BlackBerry/i)
-  || window.navigator.userAgent.match(/webOS/i)) {
-    alert('You might have a better experience playing "Get Fluffy!" on a desktop browser.');
-    //document.body.className+=' mobile';
-    //alert('Due to the lack of a physical keyboard, playing "Get Fluffy!" on mobile devices may be less than optimal\nnavigator.userAgent="'+navigator.userAgent+'"');
-  //} else {
-    //alert('False - Mobile - ' + navigator.userAgent);
-    //return false;
-  }
-  if(window.navigator.userAgent.match(/Tablet/i)
+  || window.navigator.userAgent.match(/webOS/i)
+  || window.navigator.userAgent.match(/Tablet/i)
   || window.navigator.userAgent.match(/iPad/i)
   || window.navigator.userAgent.match(/Nexus 7/i)
   || window.navigator.userAgent.match(/Nexus 10/i)
   || window.navigator.userAgent.match(/KFAPWI/i)) {
     alert('You might have a better experience playing "Get Fluffy!" on a desktop browser.');
-    //document.body.className-=' mobile';
-    //document.body.className+=' tablet';
-    //alert('Due to the lack of a physical keyboard, playing "Get Fluffy!" on tablets may be less than optimal\nnavigator.userAgent="'+navigator.userAgent+'"');
-  //} else {
-    //alert('False - Tablet - ' + navigator.userAgent);
-    //return false;
   }
 }
